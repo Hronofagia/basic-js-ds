d4579dea99dd5018be410c22b432fd27fd473f74
Nastassia Khamitsevich <hronofaqia@gmail.com>
2022-10-15 21:33:38 +0300

    feat: add solution stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  
+
+  constructor() {
+    this.miy = []
+  }
+
+  push(element) {
+    this.miy.push(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.miy.pop()
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.miy[this.miy.length -1]
   }
 }
 
